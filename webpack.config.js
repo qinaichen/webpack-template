6f013f77ca92e660f74b598b61467c52c56ccb03
qinaichen <455905502@qq.com>
2024-07-01 14:21:01 +0800

    多页面自动载入

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,52 @@
 const path = require("path");
+const Glob = require("glob");
 const htmlWebpackPlugin = require("html-webpack-plugin");
+
+
+
+
+function getEntry(globPath){
+    const entries = {main: './src/index.js'};
+    Glob.sync(`${globPath}.js`).forEach(entry => {
+        const basename = path.basename(entry,path.extname(entry));
+        const pathname = path.dirname(entry);
+        entries[basename] = `${pathname}/${basename}`;
+    })
+}
+
+
+function getHtmlWebpackPlugins(globPath){
+    const htmlPlugins = [];
+    Glob.sync(`${globPath}.html`).forEach(entry => {
+        const basename = path.basename(entry,path.extname(entry));
+        const pathname = path.dirname(entry);
+        htmlPlugins.push(new htmlWebpackPlugin({
+            title: basename,
+            filename: `${basename}.html`,
+            template: `${pathname}/${basename}.html`,
+            chunks: [basename,'main'],
+            minify: true,
+        }))
+    })
+    return htmlPlugins;
+}
+
+
+function getPages(pagePath){
+    const entries = getEntry(pagePath);
+    const htmlPlugins = getHtmlWebpackPlugins(pagePath);
+    return {
+        entries,
+        htmlPlugins
+    }
+}
+
+const {entries,htmlPlugins} =getPages('./public/**/*')
+
 module.exports = {
     mode: "development",
     devtool: "inline-source-map",
-    entry: "./src/index.js",
+    entry: entries,
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, "dist"),
@@ -22,15 +65,24 @@ module.exports = {
             {
                 test: /\.(woff|woff2|eot|tff|otf)$/i,
                 type: "asset/resource",
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use:{
+                    loader: "babel-loader",
+                    options: {
+                        presets:['@babel/preset-env'],
+                    }
+                }
             }
         ]
     },
     plugins: [
-        new htmlWebpackPlugin({
-            template: path.resolve(__dirname, "./public/index.html"),
-        })
+        ...htmlPlugins
     ],
     devServer: {
         port: 3000,
+        hot: true,
     }
 }
